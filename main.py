333f46e1e14ba7e9e6aa6b653d4a7afd5b236de0
Kartik Agarwal <57009207+kartikagarwal9@users.noreply.github.com>
2020-11-05 00:57:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+91 #mobile number' , 'First Try' , 14 , 53)    #14 and 53 are time in hours and minute you want to send the message , change in whatever you like
+
+
+#Read More On - https://www.studytonight.com/post/whatsapp-automation-to-send-message-using-python
